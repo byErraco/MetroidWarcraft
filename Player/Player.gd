b9f633eaf32aba681f71d94b450b8f17d271d32f
byErraco <erraco.wow@gmail.com>
2020-06-05 19:52:08 -0400

    first commit

---
+++ Player/Player.gd
@@ -0,0 +1,96 @@
+extends KinematicBody2D
+
+export (int) var  ACCELERATION = 512
+export (int) var  MAX_SPEED = 64
+export (float) var  FRICTION = 0.25
+
+export (int)  var GRAVITY = 250
+export (int)  var MAX_SLOPE_ANGLE = 46
+export (int) var JUMP_FORCE =  128
+
+var motion = Vector2.ZERO
+var snap_vector = Vector2.ZERO
+var just_jumped = false
+onready var sprite = $Sprite
+onready var spriteAnimator = $SpriteAnimator
+
+func _physics_process(delta):
+	just_jumped = false
+	var input_vector = get_input_vector()
+	apply_horizontal_force(input_vector,delta)
+	apply_friction(input_vector)
+	update_snap_vector()
+	jump_check()
+	apply_gravity(delta)
+	update_animations(input_vector)
+	move()
+
+func get_input_vector():
+	var input_vector =  Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	return input_vector
+
+func apply_horizontal_force(input_vector, delta):
+	if input_vector.x != 0:
+		motion.x += input_vector.x * ACCELERATION * delta
+		motion.x = clamp(motion.x, -MAX_SPEED,MAX_SPEED)
+
+func apply_friction(input_vector):
+	if input_vector.x == 0 and is_on_floor():
+		motion.x = lerp(motion.x, 0, FRICTION)
+		
+
+func update_snap_vector():
+	if is_on_floor():
+		snap_vector = Vector2.DOWN
+
+func jump_check():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -JUMP_FORCE
+			just_jumped = true
+			snap_vector = Vector2.ZERO
+
+	else:
+		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE/2;
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		print("ola")
+		motion.y += GRAVITY * delta
+		motion.y = min(motion.y, JUMP_FORCE)
+		
+func update_animations(input_vector):
+	if input_vector.x != 0:
+		sprite.scale.x = sign(input_vector.x)
+		spriteAnimator.play("Run")
+	else:
+		spriteAnimator.play("Idle")
+	
+	if not is_on_floor():
+		print("hola")
+		spriteAnimator.play("Jump2")
+
+func move():
+	var was_on_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var last_position = position
+	var last_motion = motion
+	
+	motion = move_and_slide_with_snap(motion, snap_vector * 4, Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE))
+	
+	#aterrizar
+	if was_on_air and is_on_floor():
+		motion.x = last_motion.x
+	
+	
+	#Dejar el suelo
+	if was_on_floor and not is_on_floor() and not just_jumped:
+		motion.y = 0
+		position.y = last_position.y
+		
+	# evitar q se deslizamiento
+	if is_on_floor() and get_floor_velocity().length() == 0 and abs(motion.x) < 1:
+		position.x = last_position.x
